5269024fd7e9d48795f89cbebb84cea94bedb821
IdoYehe <idoye@campus.technion.ac.il>
2018-05-02 18:20:15 +0300

    adding turing on anf off the lottery schedulder

---
+++ HW_2/User_Files/hw2_test2.c
@@ -0,0 +1,20 @@
+#include <limits.h>
+#include <stdlib.h>
+#include <sched.h>
+
+#include "hw2_syscalls.h"
+#include "test_utilities.h"
+
+int main() {
+
+	start_orig_scheduler();
+	set_max_tickets(25);
+	ASSERT_TEST(start_lottery_scheduler() == 0);
+	ASSERT_TEST(start_lottery_scheduler() == -1 && errno == EINVAL);
+	ASSERT_TEST(start_orig_scheduler() == 0);
+	ASSERT_TEST(start_orig_scheduler() == -1 && errno == EINVAL);
+
+
+
+	return 0;
+}
