c78855ee57b957a3e7087bc2d391cc17e5878774
IdoYehe <idoye@campus.technion.ac.il>
2018-05-26 21:30:47 +0300

    fix Factory.h  to match original file

--- HW_3/Factory.h
+++ HW_3/Factory.h
@@ -106,45 +106,32 @@ public:
     pthread_t removeBuyerIDFromMap(int id);
 
     Factory();
-
     ~Factory();
 
-    void startProduction(int num_products, Product *products, unsigned int id);
-
-    void produce(int num_products, Product *products);
-
+    void startProduction(int num_products, Product* products, unsigned int id);
+    void produce(int num_products, Product* products);
     void finishProduction(unsigned int id);
 
     void startSimpleBuyer(unsigned int id);
-
     int tryBuyOne();
-
     int finishSimpleBuyer(unsigned int id);
 
-    void startCompanyBuyer(int num_products, int min_value, unsigned int id);
-
+    void startCompanyBuyer(int num_products, int min_value,unsigned int id);
     std::list<Product> buyProducts(int num_products);
-
-    void returnProducts(std::list<Product> products, unsigned int id);
-
+    void returnProducts(std::list<Product> products,unsigned int id);
     int finishCompanyBuyer(unsigned int id);
 
-    void startThief(int num_products, unsigned int fake_id);
-
-    int stealProducts(int num_products, unsigned int fake_id);
-
+    void startThief(int num_products,unsigned int fake_id);
+    int stealProducts(int num_products,unsigned int fake_id);
     int finishThief(unsigned int fake_id);
 
     void closeFactory();
-
     void openFactory();
 
     void closeReturningService();
-
     void openReturningService();
 
     std::list<std::pair<Product, int>> listStolenProducts();
-
     std::list<Product> listAvailableProducts();
     };
 #endif // FACTORY_H_
