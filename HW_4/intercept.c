21d2edf0cf1614fae76a11951737a577de451d3a
IdoYehe <idoye@campus.technion.ac.il>
2018-06-13 17:41:01 +0300

    wet_4 adding comments

--- HW_4/intercept.c
+++ HW_4/intercept.c
@@ -5,12 +5,10 @@
 #include <linux/syscall.h>
 #include <linux/sched.h>
 
-
 #define RANGE_INIT 200
 #define MAX_NAME_LENGTH 16
 #define EQUAL 1
 #define NOT_EQUAL 0
-
 
 MODULE_LICENSE("GPL");
 
@@ -41,43 +39,49 @@ void** sys_call_table = NULL;
 asmlinkage long(*original_kill)(int, int);
 
 asmlinkage long kill_wet4(int pid, int sig) {
-    task_t* victim = find_task_by_pid(pid);
-
-    if (sig != SIGKILL || victim == NULL || program_name_cmp(program_name,victim->comm) == NOT_EQUAL){
+    if (sig != SIGKILL){
         return original_kill(pid, sig);
     }
+    read_lock(&tasklist_lock);
+    task_t* victim = find_task_by_pid(pid);
+    if (victim == NULL){
+        read_unlock(&tasklist_lock);
+        return original_kill(pid, sig);
+    }
+    if(program_name_cmp(program_name,victim->comm) == NOT_EQUAL) {
+        read_unlock(&tasklist_lock);
+        return original_kill(pid, sig);
+    }
+    read_unlock(&tasklist_lock);
     return -EPERM;
 }
 
-
-
 void find_sys_call_table(int scan_range) {
-
     sys_call_table =(void**) &system_utsname;
 
     int i=0;
     for (i = 0; i < scan_range; i++) {
-        if (sys_call_table[__NR_read] == sys_read) {
+        if (sys_call_table[__NR_read] == sys_read) {//sys_call_table found
             return;
         }
         sys_call_table++;
     }
-    sys_call_table = NULL;
+    sys_call_table = NULL;//re init sys_call_table pointer
 }
 
 int init_module(void) {
-    if (program_name != NULL) {
+    if (program_name != NULL) {//without program name module do nothing
         find_sys_call_table(iScanRange);
         if (sys_call_table != NULL) {
-            original_kill = sys_call_table[__NR_kill];
-            sys_call_table[__NR_kill] = kill_wet4;
+            original_kill = sys_call_table[__NR_kill];//save orig sys call kill
+            sys_call_table[__NR_kill] = kill_wet4;//put our sys call kill
         }
     }
     return 0;
 }
 
 void cleanup_module(void) {
-    if (program_name != NULL && sys_call_table != NULL) {
+    if (program_name != NULL && sys_call_table != NULL) {//restore to original ONLY if changed
         sys_call_table[__NR_kill] = original_kill;//restore original kill sys call
     }
 }
